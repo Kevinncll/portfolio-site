878c05ed9ecd873af23825b2151bbb21ba9573ae
Kevinncll <Kevinnthuong@gmail.com>
2021-03-04 15:18:06 -0500

    Comments and Font Changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 function welcome() {
     let username = prompt("What is your name?");
     alert("Nice to meet you, " + username);
+    let message = "Nice to meet you, " +username;
     document.getElementById('saywelcome').innerHTML = message;
 
 }
