31d766e17ba1a88f942e6da24b7534592cd11410
Kevinncll <Kevinnthuong@gmail.com>
2021-03-09 08:36:51 -0500

    Buttons

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,45 @@ function welcome() {
 
 //ComingSoon//
 function comingsoon() {
-    alert("This Assignment Will Be Coming Soon");
+    alert("Assignment Two Will Be Coming Soon");
+}
 
+//ComingSoon2//
+function comingsoon2() {
+    alert("Assignment Three Will Be Coming Soon");
+}
+
+//ComingSoon3//
+function comingsoon3() {
+    alert("Assignment Four Will Be Coming Soon");
+}
+
+//ComingSoon4//
+function comingsoon4() {
+    alert("Assignment Five Will Be Coming Soon");
+}
+
+//ComingSoon5//
+function comingsoon5() {
+    alert("Assignment Six Will Be Coming Soon");
+}
+
+//ComingSoon6//
+function comingsoon6() {
+    alert("Assignment Seven Will Be Coming Soon");
+}
+
+//ComingSoon7//
+function comingsoon7() {
+    alert("Assignment Eight Will Be Coming Soon");
+}
+
+//ComingSoon8//
+function comingsoon8() {
+    alert("Assignment Nine Will Be Coming Soon");
+}
+
+//ComingSoon9//
+function comingsoon9() {
+    alert("Assignment Ten Will Be Coming Soon");
 }
