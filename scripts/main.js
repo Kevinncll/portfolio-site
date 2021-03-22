5f28568b794534c336e38616fed15d222f159a56
Kevinncll <Kevinnthuong@gmail.com>
2021-03-22 14:37:55 -0400

    Nesting and Periods

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,16 @@
 function welcome() {
     let username = prompt("What is your name?");
     alert("Nice to meet you, " + username);
-    let message = "Nice to meet you, " +username;
+    let message = "Nice to meet you, " + username + ".";
     document.getElementById('saywelcome').innerHTML = message;
 
 }
 
 //ComingSoon9//
 function comingsoon9() {
-    alert("Assignment Ten Will Be Coming Soon");
+    alert("Assignment Ten Will Be Coming Soon.");
+    output = "Assignment Ten Will Be Coming Soon.";
+    document.getElementById("assignmenten").innerHTML = output;
 }
 
 //JavaScript Exercise 1//
@@ -18,13 +20,13 @@ function largernumber(){
     let num2 =  parseInt(prompt("Enter the second number: "));
     let output = ""
     if (num1 > num2){
-    output = num1;
+        output = num1;
     } else if (num1 == num2){
-    output = "The numbers entered are equal";
+        output = "The numbers entered are equal.";
     } else if (num2 > num1){
-    output = num2;
+        output = num2;
     } else {
-    output = "Invalid Output";
+        output = "Invalid Output.";
     }
     document.getElementById("largerNum").innerHTML = output;
 }
@@ -36,22 +38,23 @@ function signOfProduct(){
     var c = parseInt(prompt("Place a number"));
     var totalProduct = a * b * c;
     if (totalProduct < 0 ){
-      output = "The sign of the product is -"; 
+      output = "The sign of the product is -."; 
     }
     if (totalProduct > 0 ){
-      output = "The sign of the product is +";  
+      output = "The sign of the product is +.";  
     }
     document.getElementById("SignProduct").innerHTML = output;
 }
 
 //JavaScript Exercise 3//
 function threeNumberSort() {
-    var a = parseInt(prompt("Place a number"));
-    var b = parseInt(prompt("Place a number"));
-    var c = parseInt(prompt("Place a number"));
+    var a = parseInt(prompt("Insert a number."));
+    var b = parseInt(prompt("Insert a number."));
+    var c = parseInt(prompt("Insert a number."));
     let array = [a, b, c];
-    array.sort(function(a, b, c){return a - b});
-    output = "The order from least to greatest is, " + array;
+    array.sort(function(a, b, c){return a - b});{
+        output = "The order from least to greatest is, " + array + ".";
+    }
     document.getElementById("LeastGreat").innerHTML = output;
 }
 
@@ -61,7 +64,7 @@ function SumOfThrees() {
     for (let i = 0; i < 1000; i++) {
         ! (i % 3) && (sum += i)
     }
-    output = "The sum of three is, " + sum;
+    output = "The sum of three is, " + sum + ".";
     document.getElementById("SumThree").innerHTML = output;
 }
 
@@ -73,26 +76,26 @@ function WhileThrees(){
          sum += i;
          i = i + 3;
      }
-     output = "The while of three is, " + sum;
+     output = "The while of three is, " + sum + ".";
      document.getElementById("WhileThree").innerHTML = output;
 }
 
 //JavaScript Exercise 6//
 function tenFour() {
-    let multipleOf4or10 = 0;
+    let multiples410 = 0;
      for(i = 1; i<101; i = i +1){
          if(i % 4 === 0 && i % 10 === 0){
-            multipleOf4or10 += (' TenFour! ');
+            multiples410 += (' TenFour, ');
          }
          else if(i % 4 === 0){
-             multipleOf4or10 += (' Four! ');
+            multiples410 += (' Four, ');
          }
          else if(i % 10 === 0){
-             multipleOf4or10 += ('Ten!');
+            multiples410 += ('Ten, ');
          }
-         else(multipleOf4or10 += ' ' + i + ' ');
+         else(multiples410 += ' ' + i + ' ');
      }
-    alert (multipleOf4or10);
+    alert (multiples410);
 }
 
 //JavaScript Exercise 7//
@@ -100,10 +103,9 @@ function centuryFromYear() {
     let userYear = prompt ('Want to know what century any year is in? Enter a year!', '2021');
     let century = 0;
     century = Math.floor(userYear/100+1);
-        if (toString(century)[-1] == 1) /*&& toString(century)[-1] !=11)*/{ 
-           century = toString(century) + 'st'
+        if (toString(century)[-1] == 1){ 
     }
-    output = "The year you entered is in the century of " + century;
+    output = "The year you entered is in the century of " + century + ".";
     document.getElementById("Century").innerHTML = output;  
 }
 
@@ -115,12 +117,12 @@ function thirdAngle() {
     var totalSum = 0;
     if ((a + b) < 180){
     totalSum = 180 - (a + b); 
-    alert ("The angle that is missing is, " + totalSum );
-    output = "The angle that is missing is, " + totalSum;
+        alert ("The angle that is missing is, " + totalSum + ".");
+        output = "The angle that is missing is, " + totalSum + ".";
     }
     else if ((a + b) > 180){
-    alert ("Invalid. Enter two angles of a triangle."); 
-    output = "Invalid. Enter two angles of a triangle.";
+        alert ("Invalid. Enter two angles of a triangle."); 
+        output = "Invalid. Enter two angles of a triangle.";
     }
     document.getElementById("Triangle").innerHTML = output;  
 }
